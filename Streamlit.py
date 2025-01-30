b28ccd199e88fa89a6d3e8a62f6b640f83e7288a
tvazapphilly <145995246+tvazapphilly@users.noreply.github.com>
2025-01-30 22:10:32 +0000

    updated

--- Streamlit.py
+++ Streamlit.py
@@ -2,9 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sn
 import streamlit as st
+import numpy as np
 
-df = pd.read_csv("high_popularity_spotify_data[1].csv")
-audio_features = ['energy', 'tempo', 'danceability','loudness','liveliness','valence','speechiness','instrumentalness','mode','key','duration_ms','acousticness']
+df1 = pd.read_csv("high_popularity_spotify_data[1].csv")
+df2 = pd.read_csv("low_popularity_spotify_data[1].csv")
+df = df1+df2
+
+audio_features = ['energy', 'tempo', 'danceability','loudness','liveness','valence','speechiness','instrumentalness','mode','key','duration_ms','acousticness']
+
 
 def scatter_plot(x,y, x_label, y_label):
     fig, ax = plt.subplots()
@@ -22,10 +27,22 @@ def correlation_heatmap():
     ax.set_title('Correlation Heatmap')
     st.pyplot(fig)
 
-st.title('Comparing Categories: What makes a song popular?')
+st.title('What makes a song popular?')
+multi = "- Pick 2 different audio features to see how they compare in making a song popular. "
+st.markdown("Comparing Audio Features:")
+st.markdown(multi)
 
-x_column = st.selectbox('Select the feature for X axis', audio_features)
-y_column = st.selectbox('Select the feature for Y axis', audio_features)
+x_column = st.selectbox('Select the audio feature for the X axis', audio_features)
+y_column = st.selectbox('Select the audio feature for the Y axis', audio_features)
 
 scatter_plot(x_column, y_column, x_column,  y_column)
+st.markdown("Next, I am going to show all of the features to see which ones have the greatest correlation. ")
 correlation_heatmap()
+st.markdown("From this you can conclude that...")
+st.markdown("- energy and loudness(0.8)")
+st.markdown("- danceability and valence(0.4)")
+st.markdown("- energy and valence(0.4)")
+st.markdown("- loudness and valence(0.4)")
+st.markdown("have the greatest correlations with each other.")
+st.markdown("*Let's take a closer look at each of these features*")
+
